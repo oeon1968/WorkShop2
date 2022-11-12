608f4a85ae273caba319eef2b78ebb777507ccba
oeon1968 <oeon1968@gmail.com>
2022-11-12 17:43:15 +0100

    findAll method

--- pl.workshop2/src/main/java/workshop2/UserDao.java
+++ pl.workshop2/src/main/java/workshop2/UserDao.java
@@ -1,53 +1,84 @@
 package workshop2;
 /* commit and push*/
 import java.sql.*;
+import java.util.Arrays;
 
 public class UserDao {
-
-
-    public User createUser(User user){
+    public User createUser(User user) {
         String insertStr = "INSERT into users(name, email, password)\n" +
                 "values (?, ?, ?)";
 
-        try( Connection userConnection = DriverManager.getConnection(
+        try (Connection userConnection = DriverManager.getConnection(
                 DbUtil.urlString,
                 DbUtil.getDbUser(),
-                DbUtil.getDbPass())){
-           user.id = DbUtil.insert(userConnection, insertStr, user.name, user.email, user.password);
-        } catch (SQLException sqlException){
+                DbUtil.getDbPass())) {
+            user.id = DbUtil.insert(userConnection, insertStr, user.name, user.email, user.password);
+        } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
         return user;
     }
 
-    public User readUser(int userId){
+    public User readUser(int userId) {
         String selectSTR = "SELECT name, email, password FROM users WHERE id = ?";
         User user = new User();
-        try( Connection userConnection = DriverManager.getConnection(
+        try (Connection userConnection = DriverManager.getConnection(
                 DbUtil.urlString,
                 DbUtil.getDbUser(),
-                DbUtil.getDbPass())){
+                DbUtil.getDbPass())) {
             user = DbUtil.getUser(userConnection, selectSTR, userId);
-        }
-         catch (SQLException sqlException){
+        } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
 
         return user;
     }
 
-    public void updateUser(User user){
+    public void updateUser(User user) {
         String query = "UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?";
-        try( Connection userConnection = DriverManager.getConnection(
+        try (Connection userConnection = DriverManager.getConnection(
                 DbUtil.urlString,
                 DbUtil.getDbUser(),
-                DbUtil.getDbPass())){
-                DbUtil.update(userConnection,query, user.getId(),
-                    user.getName(),user.getEmail(), user.getPassword());
-        }
-        catch (SQLException sqlException){
+                DbUtil.getDbPass())) {
+            DbUtil.update(userConnection, query, user.getId(),
+                    user.getName(), user.getEmail(), user.getPassword());
+        } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
     }
 
+    public static User[] findAll() {
+        User[] users = new User[0];
+        ResultSet rsUsers;
+        String selectSTR = "SELECT id, name, email, password FROM users";
+
+        try (Connection userConnection = DriverManager.getConnection(
+                DbUtil.urlString,
+                DbUtil.getDbUser(),
+                DbUtil.getDbPass())) {
+            PreparedStatement stmt = userConnection.prepareStatement(selectSTR,
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY);
+            rsUsers = stmt.executeQuery();
+
+            while (rsUsers.next()) {
+                users = expandArray(users);
+                User tmpUser = new User();
+                tmpUser.setId(rsUsers.getInt("id"));
+                tmpUser.name = rsUsers.getString("name");
+                tmpUser.email = rsUsers.getString("email");
+                tmpUser.password = rsUsers.getString("password");
+                users[users.length-1] = tmpUser;
+                System.out.println(users[users.length -1].getName());
+
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return users;
+    }
+
+    private static User[] expandArray (User[] sourceArr){
+        return Arrays.copyOf(sourceArr, sourceArr.length+1);
+    }
 }
--- pl.workshop2/src/main/java/workshop2/mainUserDao.java
+++ pl.workshop2/src/main/java/workshop2/mainUserDao.java
@@ -1,17 +1,17 @@
 package workshop2;
 /* commit and push*/
 public class mainUserDao {
-
+    public static UserDao myUserDao = new UserDao();
     public static void main(String[] args) {
 
-        String tmpName = "Artur 2";
+       String tmpName = "Artur 2";
 
-        UserDao myUserDao = new UserDao();
+        User[] myUsers;
         User myUser = new User();
-        /*myUser.setName(tmpName);
+        myUser.setName(tmpName);
         myUser.setEmail(tmpName+"@email.pl");
         myUser.setPassword(tmpName);
-        myUser = myUserDao.createUser(myUser);*/
+        myUser = myUserDao.createUser(myUser);
 
 // Można wykorzystać metodę inicjującą obiekt user:
         //user() - create user
@@ -22,6 +22,7 @@ public class mainUserDao {
         myUser.setEmail("dariusz@email.pl");
 
         myUserDao.updateUser(myUser);
+        myUsers = UserDao.findAll();
 
 
     }
